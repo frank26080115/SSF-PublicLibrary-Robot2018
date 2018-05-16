78713de790c37d98d07340c042a152298bd423d6
Frank Zhao <frank.zhao.main@gmail.com>
2018-05-15 20:04:23 -0700

    single sensor test rig

---
+++ ArduinoSketchbook/SingleSensorTestRig/SingleSensorTestRig.ino
@@ -0,0 +1,26 @@
+#include <BookWorm.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  BookWorm.begin();
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int x, y;
+  BookWorm.setEmittersOff();
+  delay(50);
+  x = BookWorm.readSensorRightSide();
+  BookWorm.setEmittersOn();
+  delay(50);
+  y = BookWorm.readSensorRightSide();
+  if ((y - x) > 50)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else
+  {
+    digitalWrite(13, LOW);
+  }
+}
